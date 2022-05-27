327fe45fae98f97d093d6c66d11f7e0274947717
Lucas Starling <starluc@amazon.com>
2022-05-26 22:39:09 -0300

    Dev(modules-cli): Command structure

--- src/modules/clicontroller.py
+++ src/modules/clicontroller.py
@@ -1,5 +1,9 @@
+from ast import arguments
 import configparser
+from hashlib import new
 import os
+import sys
+from typing import List
 
 
 class CLI:
@@ -22,3 +26,90 @@ class CLI:
     def save_configs(self):
         with open(self._config_file, 'w') as configfile:
                 self.config.write(configfile)
+
+class Command:
+    def __init__(self, handle:str):
+        self.handle = handle
+        self.help = handle + ' help'
+        self.exec = self.help
+        self.argc = 0
+        self.argv = []
+        self.sub_commands = {}
+        self.flags = {}
+        self.shorts = {}
+        self.flag_has_parameter = {}
+        self.shell_prefix = ""
+
+    def help(self):
+        print(self.help)
+    
+    def run(self):
+        self.argv = sys.argv
+        if self.handle != self.consume():
+            self.help()
+        self.parse()
+        
+    def parse(self):
+        if self.super():
+            if len(self.argv) == 0:
+                self.shell()
+            sub_handle = self.consume()
+            if sub_handle not in self.sub_commands:
+                self.help()
+                return
+            sub_command = self.sub_commands[sub_handle]
+            sub_command.parse(self.argv)
+        else:
+            if len(self.argv) < self.argc:
+                self.help()
+                return
+            arguments = self.consume(self.argc)
+            for argument in arguments:
+                if self.is_flag(argument):
+                    self.help()
+                    return
+            
+            
+    
+    def parse_flags(self):
+        argument = self.consume()
+        while(argument != ''):
+            if not self.is_flag(argument):
+                self.help()
+                return
+            if argument in self.shorts:
+                argument = self.shorts[argument]
+            if argument not in self.flags:
+                self.help()
+                return
+            flag_exec = self.flags[argument]
+            if self.flag_has_parameter[argument]:
+                flag_exec(self.consume)
+            flag_exec()
+
+
+    def shell(self):
+        pass
+
+    def super(self) -> bool:
+        return len(self.sub_commands) == 0
+    
+    def is_flag(self, argument:str) -> bool:
+        return argument[0] == '-'
+
+    def consume(self) -> str:
+        argument = ''
+        if len(self.argv) > 0:
+            argument = self.argv[0]
+            self.argv = self.argv[1:]
+        return argument
+
+    def get_arguments(self) -> List[str]:
+        arguments = []
+        for _ in range(self.argc):
+            arguments.append(self.consume())
+        return arguments
+
+
+
+
