84b66ae5313608833398470e8e96cff54f023687
Lucas Caetano <lucasclopesr@gmail.com>
2022-05-28 13:16:37 -0300

    Refactor tasks and add unit tests

--- src/modules/task.py
+++ src/modules/task.py
@@ -1,8 +1,11 @@
+import typing
+
 class Task:
-    def __init__(self, task_id, task_name, status, creation_date, label,
-            board_id, time_worked=0, priority=0):
-        self.task_id       = task_id
-        self.task_name     = task_name
+    def __init__(self, task_id: int, task_name: str, status: str,
+            creation_date: str, label: str, board_id: int, time_worked: int = 0,
+            priority: int =0):
+        self.id       = task_id
+        self.name     = task_name
         self.status        = status
         self.creation_date = creation_date
         self.label         = label
@@ -10,16 +13,20 @@ class Task:
         self.time_worked   = time_worked
         self.priority      = priority
 
-    def print_task(self):
-        print("Task details")
-        print("Name: ", self.task_name)
-        print("Status: ", self.status)
-        print("Creation date: ", self.creation_date)
-        print("Label: ", self.label)
-        print("Priority: ", self.priority)
-        print("Time Worked in this task: ", self.time_worked, "\n")
+    def update_status(self, new_status: str):
+        self.status = new_status
 
-        return
+    def get_task_details(self):
+        return {
+                "id": self.id,
+                "name": self.name,
+                "status": self.status,
+                "creation_date": self.creation_date,
+                "label": self.label,
+                "board_id": self.board_id,
+                "time_worked": self.time_worked,
+                "priority": self.priority
+        }
 
     def start_working(self):
         pass
@@ -27,3 +34,14 @@ class Task:
     def stop_working(self):
         pass
 
+# Printing should be extracted to CLI
+#    def print_task(self):
+#        print("Task details")
+#        print("Name: ", self.name)
+#        print("Status: ", self.status)
+#        print("Creation date: ", self.creation_date)
+#        print("Label: ", self.label)
+#        print("Priority: ", self.priority)
+#        print("Time Worked in this task: ", self.time_worked, "\n")
+#
+#        return
