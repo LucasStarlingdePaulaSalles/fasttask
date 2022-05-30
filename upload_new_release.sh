7e3b99a4e9fab5703ea94402f2ecd5df2edfaa45
LucasStarlingdePaulaSalles <lucastarling1@gmail.com>
2022-05-29 23:52:17 -0300

    New release script

---
+++ upload_new_release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Did you recall updating the version on setup.py?"
+sleep 3
+rm -rf dist
+rm -rf fasttask.egg-info
+python -m build
+python -m twine check dist/*
+python -m twine upload dist/* --verbose
