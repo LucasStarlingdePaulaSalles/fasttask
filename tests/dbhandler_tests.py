c07e1d47604e1674c8d2d80aa5b1cb088e0973fe
yasminaraujo <ygaraujo98@gmail.com>
2022-06-25 16:42:44 -0300

    create unit tests for board taks and dbhandler

--- tests/dbhandler_tests.py
+++ tests/dbhandler_tests.py
@@ -5,6 +5,8 @@ from src.modules.dbhandler import DBHandler
 class TestDBHandler(unittest.TestCase):
     def setUp(self):
         self.db = DBHandler()
+        boardTest = self.db.create_board(board_name = "test_boardTest", board_label =
+        "test")
 
     def test_initialize_db(self):
         pass
@@ -21,31 +23,74 @@ class TestDBHandler(unittest.TestCase):
         self.assertTrue(len(board.get_all_tasks()) == 0)
 
     def test_delete_board(self):
-        board = self.db.create_board(board_name = "test_board", board_label =
-        "test")
+        boardsCount = len( self.db.get_boards())
+
         board_2 = self.db.create_board(board_name = "test_board_2", board_label =
         "test")
 
         boards = self.db.get_boards()
-        self.assertTrue(len(boards) == 2)
 
-        self.db.delete_board(board_id = board_2.get_board_id())
+        self.assertTrue(len(boards) == boardsCount + 1)
+
+        self.db.delete_board(board_id = board_2.board_id)
         boards = self.db.get_boards()
-        self.assertTrue(len(boards) == 1)
+        self.assertTrue(len(boards) == boardsCount)
 
     def test_create_task_in_board(self):
-        pass
+        board_3 = self.db.create_board(board_name = "test_board_3", board_label =
+        "test")
 
-    def test_create_multiple_tasks_in_one_board(self):
-        pass
+        task = self.db.create_task(board_id = board_3.board_id, task_name = "task", label = "teste", priority= 0)
+        board_3.add_task(task)
+
+        self.assertTrue(self.db.get_task(task).board_id == board_3.board_id)
+
+    def test_create_multiple_tasks_in_board(self):
+        board_4 = self.db.create_board(board_name = "test_board_4", board_label =
+        "test")
+
+        task1 = self.db.create_task(board_id = board_4.board_id, task_name = "task1", label = "teste", priority= 0)
+        task2 = self.db.create_task(board_id = board_4.board_id, task_name = "task2", label = "teste", priority= 0)
+        task3 = self.db.create_task(board_id = board_4.board_id, task_name = "task3", label = "teste", priority= 0)
+        board_4.add_task(task1)
+        board_4.add_task(task2)
+        board_4.add_task(task3)
+
+        self.assertTrue(self.db.get_board(len(board_4.board_id.tasks) == 3))
 
     def test_create_multiple_tasks_in_two_board(self):
-        pass
+        board_5 = self.db.create_board(board_name = "test_board_5", board_label =
+        "test")
+        board_6 = self.db.create_board(board_name = "test_board_6", board_label =
+        "test")
+
+        task1 = self.db.create_task(board_id = board_5.board_id, task_name = "task1", label = "teste", priority= 0)
+        task2 = self.db.create_task(board_id = board_5.board_id, task_name = "task2", label = "teste", priority= 0)
+        task3 = self.db.create_task(board_id = board_6.board_id, task_name = "task3", label = "teste", priority= 0)
+
+        board_5.add_task(task1)
+        board_5.add_task(task2)
+        board_6.add_task(task3)
+
+        self.assertTrue(self.db.get_board(len(board_5.board_id.tasks) == 2))
+        self.assertTrue(self.db.get_board(len(board_6.board_id.tasks) == 1))
 
     def test_update_task(self):
-        pass
+        board_7 = self.db.create_board(board_name = "test_board_7", board_label =
+        "test")
+
+        task = self.db.create_task(board_id = board_7.board_id, task_name = "task1", label = "teste", priority= 0)
+        board_7.add_task(task)
+
+        assertTrue(self.db.update_task(task_id= task, new_status= "doing"))
 
     def test_delete_task(self):
-        pass
+        board_8 = self.db.create_board(board_name = "test_board_8", board_label =
+        "test")
+
+        task = self.db.create_task(board_id = board_8.board_id, task_name = "task1", label = "teste", priority= 0)
+        board_8.add_task(task)
+
+        assertTrue(self.db.delete_task(task_id= task))
 
 
